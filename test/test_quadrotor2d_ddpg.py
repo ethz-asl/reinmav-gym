c76cf63ead38981269a9e0cbd1cf54c592ea8f48
inkyusa <enddl22@gmail.com>
2019-02-07 16:29:29 +0100

    typo..

--- test/test_quadrotor2d_ddpg.py
+++ test/test_quadrotor2d_ddpg.py
@@ -5,7 +5,7 @@
 #it under the terms of the GNU Lesser General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
 #(at your option) any later version.
- 
+
 #This software package is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -42,7 +42,7 @@ ENV_NAME = 'quadrotor2d-v0'
 env = gym.make(ENV_NAME)
 np.random.seed(123)
 env.seed(123)
-assert len(env.action_space.shape) == 2
+assert len(env.action_space.shape) != 2
 nb_actions = env.action_space.shape[0]
 print("nb_actions=",nb_actions)
 WINDOW_LENGTH = 1
