9ae7adf79b47bf75c4cb238a7231bde1dabad8fb
Jaeyoung-Lim <jalim@student.ethz.ch>
2019-02-08 10:35:30 +0100

    Encode state for quadrotor3d env

--- gym_reinmav/envs/quadrotor3d.py
+++ gym_reinmav/envs/quadrotor3d.py
@@ -20,6 +20,7 @@ from gym import error, spaces, utils
 from math import cos, sin, pi, atan2
 import numpy as np
 from numpy import linalg
+from gym.utils import seeding
 from pyquaternion import Quaternion
 
 class Quadrotor3D(gym.Env):
@@ -29,13 +30,16 @@ class Quadrotor3D(gym.Env):
 		self.dt = 0.01
 		self.g = np.array([0.0, 0.0, -9.8])
 
-		self.att = np.array([1.0, 0.0, 0.0, 0.0])
-		self.pos = np.array([0.3, 0.1, 1.0])
-		self.vel = np.array([3.0, 0.0, 0.0])
+		self.state = None
 
 		self.ref_pos = np.array([0.0, 0.0, 2.0])
 		self.ref_vel = np.array([0.0, 0.0, 0.0])
 
+		# Conditions to fail the episode
+		self.pos_threshold = 0.1
+		self.vel_threshold = 0.1
+
+		self.seed()
 		self.viewer = None
 		self.render_quad1 = None
 		self.render_quad2 = None
@@ -47,20 +51,54 @@ class Quadrotor3D(gym.Env):
 		self.render_ref = None
 		self.x_range = 1.0
 
+	def seed(self, seed=None):
+		self.np_random, seed = seeding.np_random(seed)
+		return [seed]
 
 	def step(self, action):
 		thrust = action[0] # Thrust command
 		w = action[1:4] # Angular velocity command
-		att_quaternion = Quaternion(self.att)
+
+		state = self.state
+		ref_pos = self.ref_pos
+		ref_vel = self.ref_vel
+
+		pos = np.array([state[0], state[1], state[2]]).flatten()
+		att = np.array([state[3], state[4], state[5], state[6]]).flatten()
+		vel = np.array([state[7], state[8], state[9]]).flatten()
+
+
+		att_quaternion = Quaternion(att)
 
 		acc = thrust/self.mass * att_quaternion.rotation_matrix.dot(np.array([0.0, 0.0, 1.0])) + self.g
 		
-		vel = self.vel
-		self.vel = vel + acc * self.dt
-		self.pos = self.pos + vel * self.dt + 0.5*acc*self.dt*self.dt
+		pos = pos + vel * self.dt + 0.5*acc*self.dt*self.dt
+		vel = vel + acc * self.dt
 		
 		q_dot = att_quaternion.derivative(w)
-		self.att = self.att + q_dot.elements * self.dt
+		att = att + q_dot.elements * self.dt
+
+		self.state = (pos[0], pos[1], pos[2], att[0], att[1], att[2], att[3], vel[0], vel[1], vel[2])
+
+		done =  linalg.norm(pos, 2) < -self.pos_threshold \
+			and  linalg.norm(pos, 2) > self.pos_threshold \
+			and linalg.norm(vel, 2) < -self.vel_threshold \
+			and linalg.norm(vel, 2) > self.vel_threshold
+		done = bool(done)
+
+		if not done:
+		    reward = (-linalg.norm(pos, 2))
+		elif self.steps_beyond_done is None:
+		    # Pole just fell!
+		    self.steps_beyond_done = 0
+		    reward = 1.0
+		else:
+		    if self.steps_beyond_done == 0:
+		        logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
+		    self.steps_beyond_done += 1
+		    reward = 0.0
+
+		return np.array(self.state), reward, done, {}
 
 	def control(self):
 		def acc2quat(desired_acc, yaw): # TODO: Yaw rotation
@@ -83,8 +121,16 @@ class Quadrotor3D(gym.Env):
 		Kv = np.array([-4.0, -4.0, -4.0])
 		tau = 0.3;
 
-		error_pos = self.pos - self.ref_pos
-		error_vel = self.vel - self.ref_vel
+		state = self.state
+		ref_pos = self.ref_pos
+		ref_vel = self.ref_vel
+
+		pos = np.array([state[0], state[1], state[2]]).flatten()
+		att = np.array([state[3], state[4], state[5], state[6]]).flatten()
+		vel = np.array([state[7], state[8], state[9]]).flatten()
+
+		error_pos = pos - ref_pos
+		error_vel = vel - ref_vel
 
 		# %% Calculate desired acceleration
 		reference_acc = np.array([0.0, 0.0, 0.0])
@@ -95,7 +141,7 @@ class Quadrotor3D(gym.Env):
 		desired_att = acc2quat(desired_acc, 0.0)
 
 		desired_quat = Quaternion(desired_att)
-		current_quat = Quaternion(self.att)
+		current_quat = Quaternion(att)
 
 		error_att = current_quat.conjugate * desired_quat
 		qe = error_att.elements
@@ -108,11 +154,11 @@ class Quadrotor3D(gym.Env):
 		
 		action = np.array([thrust, w[0], w[1], w[2]])
 
-		return action 
+		return action
 
 	def reset(self):
 		print("reset")
-		#self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
+		self.state = np.array(self.np_random.uniform(low=-1.0, high=1.0, size=(10,1)))
 		return np.array(self.state)
 
 	def render(self, mode='human', close=False):
@@ -132,50 +178,58 @@ class Quadrotor3D(gym.Env):
 				curve(pos=[(0,i*unit,0), (-nunit, -i*unit, 0)],color=lcolor)
 				curve(pos=[(i*unit,0,0), (-i*unit, -nunit, 0)],color=lcolor)				
 
+		state = self.state
+		ref_pos = self.ref_pos
+		ref_vel = self.ref_vel
 
-		current_quat = Quaternion(self.att)
+		pos = np.array([state[0], state[1], state[2]]).flatten()
+		att = np.array([state[3], state[4], state[5], state[6]]).flatten()
+		vel = np.array([state[7], state[8], state[9]]).flatten()
+
+
+		current_quat = Quaternion(att)
 		x_axis = current_quat.rotation_matrix.dot(np.array([1.0, 0.0, 0.0]))
 		y_axis = current_quat.rotation_matrix.dot(np.array([0.0, 1.0, 0.0]))
 		z_axis = current_quat.rotation_matrix.dot(np.array([0.0, 0.0, 1.0]))
 
 		if self.viewer is None:
 			self.viewer = canvas(title='Quadrotor 3D', width=640, height=480, center=vector(0, 0, 2), forward=vector(1, 1, -0.5), up=vector(0, 0, 1), background=color.white, range=4.0, autoscale = False)
-			self.render_quad1 = box(canvas = self.viewer, pos=vector(self.pos[0],self.pos[1],0), axis=vector(x_axis[0],x_axis[1],x_axis[2]), length=0.2, height=0.05, width=0.05)
-			self.render_quad2 = box(canvas = self.viewer, pos=vector(self.pos[0],self.pos[1],0), axis=vector(y_axis[0],y_axis[1],y_axis[2]), length=0.2, height=0.05, width=0.05)
-			self.render_rotor1 = cylinder(canvas = self.viewer, pos=vector(self.pos[0],self.pos[1],0), axis=vector(0.01*z_axis[0],0.01*z_axis[1],0.01*z_axis[2]), radius=0.2, color=color.cyan, opacity=0.5)
-			self.render_rotor2 = cylinder(canvas = self.viewer, pos=vector(self.pos[0],self.pos[1],0), axis=vector(0.01*z_axis[0],0.01*z_axis[1],0.01*z_axis[2]), radius=0.2, color=color.cyan, opacity=0.5)
-			self.render_rotor3 = cylinder(canvas = self.viewer, pos=vector(self.pos[0],self.pos[1],0), axis=vector(0.01*z_axis[0],0.01*z_axis[1],0.01*z_axis[2]), radius=0.2, color=color.cyan, opacity=0.5)
-			self.render_rotor4 = cylinder(canvas = self.viewer, pos=vector(self.pos[0],self.pos[1],0), axis=vector(0.01*z_axis[0],0.01*z_axis[1],0.01*z_axis[2]), radius=0.2, color=color.cyan, opacity=0.5)
-			self.render_velocity = pointer = arrow(pos=vector(self.pos[0],self.pos[1],0), axis=vector(self.vel[0],self.vel[1],self.vel[2]), shaftwidth=0.05, color=color.green)
-			self.render_ref = sphere(canvas = self.viewer, pos=vector(self.ref_pos[0], self.ref_pos[1], self.ref_pos[2]), radius=0.02, color=color.blue, make_trail = True)
+			self.render_quad1 = box(canvas = self.viewer, pos=vector(pos[0],pos[1],0), axis=vector(x_axis[0],x_axis[1],x_axis[2]), length=0.2, height=0.05, width=0.05)
+			self.render_quad2 = box(canvas = self.viewer, pos=vector(pos[0],pos[1],0), axis=vector(y_axis[0],y_axis[1],y_axis[2]), length=0.2, height=0.05, width=0.05)
+			self.render_rotor1 = cylinder(canvas = self.viewer, pos=vector(pos[0],pos[1],0), axis=vector(0.01*z_axis[0],0.01*z_axis[1],0.01*z_axis[2]), radius=0.2, color=color.cyan, opacity=0.5)
+			self.render_rotor2 = cylinder(canvas = self.viewer, pos=vector(pos[0],pos[1],0), axis=vector(0.01*z_axis[0],0.01*z_axis[1],0.01*z_axis[2]), radius=0.2, color=color.cyan, opacity=0.5)
+			self.render_rotor3 = cylinder(canvas = self.viewer, pos=vector(pos[0],pos[1],0), axis=vector(0.01*z_axis[0],0.01*z_axis[1],0.01*z_axis[2]), radius=0.2, color=color.cyan, opacity=0.5)
+			self.render_rotor4 = cylinder(canvas = self.viewer, pos=vector(pos[0],pos[1],0), axis=vector(0.01*z_axis[0],0.01*z_axis[1],0.01*z_axis[2]), radius=0.2, color=color.cyan, opacity=0.5)
+			self.render_velocity = pointer = arrow(pos=vector(pos[0],pos[1],0), axis=vector(vel[0],vel[1],vel[2]), shaftwidth=0.05, color=color.green)
+			self.render_ref = sphere(canvas = self.viewer, pos=vector(ref_pos[0], ref_pos[1], ref_pos[2]), radius=0.02, color=color.blue, make_trail = True)
 			grid_xy = make_grid(5, 100)
-		if self.pos is None: return None
+		if self.state is None: return None
 
-		self.render_quad1.pos.x = self.pos[0]
-		self.render_quad1.pos.y = self.pos[1]
-		self.render_quad1.pos.z = self.pos[2]
-		self.render_quad2.pos.x = self.pos[0]
-		self.render_quad2.pos.y = self.pos[1]
-		self.render_quad2.pos.z = self.pos[2]
+		self.render_quad1.pos.x = pos[0]
+		self.render_quad1.pos.y = pos[1]
+		self.render_quad1.pos.z = pos[2]
+		self.render_quad2.pos.x = pos[0]
+		self.render_quad2.pos.y = pos[1]
+		self.render_quad2.pos.z = pos[2]
 		rotor_pos = 0.5*x_axis
-		self.render_rotor1.pos.x = self.pos[0] + rotor_pos[0]
-		self.render_rotor1.pos.y = self.pos[1] + rotor_pos[1]
-		self.render_rotor1.pos.z = self.pos[2] + rotor_pos[2]
+		self.render_rotor1.pos.x = pos[0] + rotor_pos[0]
+		self.render_rotor1.pos.y = pos[1] + rotor_pos[1]
+		self.render_rotor1.pos.z = pos[2] + rotor_pos[2]
 		rotor_pos = (-0.5)*x_axis
-		self.render_rotor2.pos.x = self.pos[0] + rotor_pos[0]
-		self.render_rotor2.pos.y = self.pos[1] + rotor_pos[1]
-		self.render_rotor2.pos.z = self.pos[2] + rotor_pos[2]
+		self.render_rotor2.pos.x = pos[0] + rotor_pos[0]
+		self.render_rotor2.pos.y = pos[1] + rotor_pos[1]
+		self.render_rotor2.pos.z = pos[2] + rotor_pos[2]
 		rotor_pos = 0.5*y_axis
-		self.render_rotor3.pos.x = self.pos[0] + rotor_pos[0]
-		self.render_rotor3.pos.y = self.pos[1] + rotor_pos[1]
-		self.render_rotor3.pos.z = self.pos[2] + rotor_pos[2]
+		self.render_rotor3.pos.x = pos[0] + rotor_pos[0]
+		self.render_rotor3.pos.y = pos[1] + rotor_pos[1]
+		self.render_rotor3.pos.z = pos[2] + rotor_pos[2]
 		rotor_pos = (-0.5)*y_axis
-		self.render_rotor4.pos.x = self.pos[0] + rotor_pos[0]
-		self.render_rotor4.pos.y = self.pos[1] + rotor_pos[1]
-		self.render_rotor4.pos.z = self.pos[2] + rotor_pos[2]
-		self.render_velocity.pos.x = self.pos[0]
-		self.render_velocity.pos.y = self.pos[1]
-		self.render_velocity.pos.z = self.pos[2]
+		self.render_rotor4.pos.x = pos[0] + rotor_pos[0]
+		self.render_rotor4.pos.y = pos[1] + rotor_pos[1]
+		self.render_rotor4.pos.z = pos[2] + rotor_pos[2]
+		self.render_velocity.pos.x = pos[0]
+		self.render_velocity.pos.y = pos[1]
+		self.render_velocity.pos.z = pos[2]
 
 		self.render_quad1.axis.x = x_axis[0]
 		self.render_quad1.axis.y = x_axis[1]	
@@ -195,9 +249,9 @@ class Quadrotor3D(gym.Env):
 		self.render_rotor4.axis.x = 0.01*z_axis[0]
 		self.render_rotor4.axis.y = 0.01*z_axis[1]
 		self.render_rotor4.axis.z = 0.01*z_axis[2]
-		self.render_velocity.axis.x = 0.5 * self.vel[0]
-		self.render_velocity.axis.y = 0.5 * self.vel[1]
-		self.render_velocity.axis.z = 0.5 * self.vel[2]
+		self.render_velocity.axis.x = 0.5 * vel[0]
+		self.render_velocity.axis.y = 0.5 * vel[1]
+		self.render_velocity.axis.z = 0.5 * vel[2]
 
 
 		self.render_quad1.up.x = z_axis[0]
@@ -208,9 +262,9 @@ class Quadrotor3D(gym.Env):
 		self.render_quad2.up.z = z_axis[2]
 
 
-		self.render_ref.pos.x = self.ref_pos[0]
-		self.render_ref.pos.y = self.ref_pos[1]
-		self.render_ref.pos.z = self.ref_pos[2]
+		self.render_ref.pos.x = ref_pos[0]
+		self.render_ref.pos.y = ref_pos[1]
+		self.render_ref.pos.z = ref_pos[2]
 
 		rate(100)
 
--- test/test_quadrotor3d.py
+++ test/test_quadrotor3d.py
@@ -27,18 +27,15 @@ from timeit import default_timer as timer
 class Environments(unittest.TestCase):
 	def test_env(self):
 		env = gym.make('quadrotor3d-v0')
-		#env.reset()
-		#for i, _ in enumerate(dict_of_list[key]):
+		env.reset()
 		start_t=timer()
 		for i,_ in enumerate(range(400)): #dt=0.01, 400*0.01=4s
-		# 	#env.render()
-			#print("============step {}============".format(i))
-			#print("step=",i)
-		 	#env.step(env.action_space.sample()) # take a random action
-		 	# action = env.control(reference)
-		    action = env.control()
-		    env.step(action) # take a random action
-		    env.render() # take a random action
+
+			action = env.control()
+			_, reward, done, _ = env.step(action) # take a random action
+			env.render() # take a random action
+			if(done):
+				env.reset()
 		end_t=timer()
 		print("simulation time=",end_t-start_t)
 		# env.plot_state()
