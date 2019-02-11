3adbecb40464d2103e9ce2a7ea6a2e484725ec4c
Jaeyoung-Lim <jalim@student.ethz.ch>
2019-02-11 21:34:14 +0100

    Improve directory structure

---
+++ gym_reinmav/envs/native/__init__.py
@@ -0,0 +1,5 @@
+from gym_reinmav.envs.reinmav_env import ReinmavEnv
+from gym_reinmav.envs.quadrotor2d import Quadrotor2D
+from gym_reinmav.envs.quadrotor2d_slungload import Quadrotor2DSlungload
+from gym_reinmav.envs.quadrotor3d import Quadrotor3D
+from gym_reinmav.envs.quadrotor3d_slungload import Quadrotor3DSlungload
