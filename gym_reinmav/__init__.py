9e0e50ce50f9085303043a38ea8c131e2cdc0689
Jaeyoung-Lim <jalim@student.ethz.ch>
2019-02-23 22:03:49 +0100

    Fix environment path

--- gym_reinmav/__init__.py
+++ gym_reinmav/__init__.py
@@ -2,27 +2,27 @@ from gym.envs.registration import register
 
 register(
     id='reinmav-v0',
-    entry_point='gym_reinmav.envs:ReinmavEnv',
+    entry_point='gym_reinmav.envs.native:ReinmavEnv',
 )
 
 register(
-	id='quadrotor2d-v0',
-	entry_point='gym_reinmav.envs:Quadrotor2D',
+    id='quadrotor2d-v0',
+    entry_point='gym_reinmav.envs.native:Quadrotor2D',
 )
 
 register(
-	id='quadrotor2d-slungload-v0',
-	entry_point='gym_reinmav.envs:Quadrotor2DSlungload',
+    id='quadrotor2d-slungload-v0',
+    entry_point='gym_reinmav.envs.native:Quadrotor2DSlungload',
 )
 
 register(
-	id='quadrotor3d-v0',
-	entry_point='gym_reinmav.envs:Quadrotor3D',
+    id='quadrotor3d-v0',
+    entry_point='gym_reinmav.envs.native:Quadrotor3D',
 )
 
 register(
-	id='quadrotor3d-slungload-v0',
-	entry_point='gym_reinmav.envs:Quadrotor3DSlungload',
+    id='quadrotor3d-slungload-v0',
+    entry_point='gym_reinmav.envs.native:Quadrotor3DSlungload',
 )
 
 register(
