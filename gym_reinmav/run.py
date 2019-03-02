e84e62f2b3b717df8656037a7452a69286c4776a
Jaeyoung-Lim <jalim@student.ethz.ch>
2019-03-02 11:23:24 +0100

    Add action / ovservation space info in env

--- gym_reinmav/run.py
+++ gym_reinmav/run.py
@@ -36,20 +36,6 @@ for env in gym.envs.registry.all():
     env_type = env._entry_point.split(':')[0].split('.')[-1]
     _game_envs[env_type].add(env.id)
 
-# reading benchmark names directly from retro requires
-# importing retro here, and for some reason that crashes tensorflow
-# in ubuntu
-_game_envs['retro'] = {
-    'BubbleBobble-Nes',
-    'SuperMarioBros-Nes',
-    'TwinBee3PokoPokoDaimaou-Nes',
-    'SpaceHarrier-Nes',
-    'SonicTheHedgehog-Genesis',
-    'Vectorman-Genesis',
-    'FinalFight-Snes',
-    'SpaceInvaders-Snes',
-}
-
 
 def train(args, extra_args):
     env_type, env_id = get_env_type(args)
