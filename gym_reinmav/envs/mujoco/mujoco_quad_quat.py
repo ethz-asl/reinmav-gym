af3ba2b7327167fbedf0a55807107a6eec80d19e
inkyusa <enddl22@gmail.com>
2019-03-18 17:13:05 +0100

    reward info testing

--- gym_reinmav/envs/mujoco/mujoco_quad_quat.py
+++ gym_reinmav/envs/mujoco/mujoco_quad_quat.py
@@ -9,12 +9,9 @@ class MujocoQuadQuaternionEnv(MujocoQuadEnv):
     def __init__(self, xml_name="quadrotor_quat.xml"):
         super(MujocoQuadQuaternionEnv, self).__init__(xml_name=xml_name)
 
-        self.goal_pos = np.array([0.0, 0.0, 1.0])
-        self.alive_bonus = 100
-
-
-
     def step(self, a):
+        goal_pos = np.array([0.0, 0.0, 1.0])
+        alive_bonus = 100
         self.do_simulation(self.clip_action(a), self.frame_skip)
         ob = self._get_obs()
         pos = ob[0:3]
@@ -25,11 +22,11 @@ class MujocoQuadQuaternionEnv(MujocoQuadEnv):
         ang_acc = ob[16:19]
         
 
-        reward_position = - linalg.norm(pos-self.goal_pos) * 10 
+        reward_position = - linalg.norm(pos-goal_pos) * 10 
         reward_linear_velocity = - linalg.norm(lin_vel) * 0.1 
         reward_angular_velocity = - linalg.norm(ang_vel) * 0.1
         reward_action = - linalg.norm(a)
-        reward_alive = self.alive_bonus
+        reward_alive = alive_bonus
 
         reward = reward_position \
                  + reward_linear_velocity \
@@ -47,18 +44,18 @@ class MujocoQuadQuaternionEnv(MujocoQuadEnv):
           'rlv': reward_linear_velocity,
           'rav': reward_angular_velocity,
           'ra': reward_action,
-          'ra2': reward_alive,
+          'rlive': reward_alive,
         }
 
         #if done=True indicates the episode has terminated and it's time to reset the environment. (For example, perhaps the pole tipped too far, or you lost your last life.) https://gym.openai.com/docs/
         done = not notdone
-
-        #If the episode is done, then add some variations to the initial state that will be exploited for the next ep. The low and high bounds empirically set.
-        
         return ob, reward, done, info
 
     def reset_model(self):
-        qpos = self.init_qpos + self.np_random.uniform(size=3, low=-0.05, high=0.05)
-        qvel = self.init_qvel + self.np_random.uniform(size=3, low=-0.01, high=0.01)
+        #If reset, then we add some variations to the initial state that will be exploited for the next ep. The low and high bounds empirically set.
+        qpos=self.init_qpos
+        qvel=self.init_qvel
+        qpos[0:3] +=self.np_random.uniform(size=3, low=-0.1, high=0.1)
+        qvel[0:3] +=self.np_random.uniform(size=3, low=-0.01, high=0.01)
         self.set_state(qpos, qvel)
         return self._get_obs()
