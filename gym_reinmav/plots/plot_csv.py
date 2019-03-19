b99675854f2e30399ecd305e42109c6478ef257b
inkyusa <enddl22@gmail.com>
2019-03-19 13:00:21 +0100

    clean up and refactorying plot script.

--- gym_reinmav/plots/plot_csv.py
+++ gym_reinmav/plots/plot_csv.py
@@ -2,58 +2,58 @@ from baselines.common import plot_util as pu
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sys,os
 
-log_path='~/workspace/reinmav-gym/gym_reinmav/example/mujoco/log/mujoco/0.0.monitor.csv'
-headers = ['r','l','t','rp','rlv','rav','ra','rlive']
-data = pd.read_csv(log_path,names=headers,skiprows=1,header=2)
-x_axis_unit="step" #"step" or "time"
-#print(data.head())
-if x_axis_unit=="time":
-	x_axis=data.t
-else:
-	x_axis=np.cumsum(data.l)
+class plotCsv():
+	def __init__(self,log_path):
+		self.log_path=log_path
+		self.headers = ['r','l','t','rp','rlv','rav','ra','rlive']
+		self.x_axis_unit="step" #"step" or "time"
+	def loadData(self):
+		self.data = pd.read_csv(self.log_path,names=self.headers,skiprows=1,header=2)
+		if self.x_axis_unit=="time":
+			self.x_axis=self.data.t
+		else:
+			self.x_axis=np.cumsum(self.data.l)
+	def plotData(self):
+		plt.figure('Main params')
+		#plt.plot(np.cumsum(data.l),pu.smooth(data.r, radius=100))
+		plt.plot(self.x_axis,pu.smooth(self.data.r, radius=100))
+		plt.plot(self.x_axis,pu.smooth(self.data.l, radius=100))
 
-plt.figure('Main params')
-#plt.plot(np.cumsum(data.l),pu.smooth(data.r, radius=100))
-plt.plot(x_axis,pu.smooth(data.r, radius=100))
-plt.plot(x_axis,pu.smooth(data.l, radius=100))
+		plt.legend(['episode mean reward','episode mean length'])
+		if self.x_axis_unit=="time":
+			plt.xlabel('Time elapsed(s)')
+		else:
+			plt.xlabel('Step')
+		plt.ylabel('reward')
+		plt.grid()
+		
+		plt.figure('Aux params')
+		plt.plot(self.x_axis,pu.smooth(self.data.rp, radius=100))
+		plt.plot(self.x_axis,pu.smooth(self.data.rlv, radius=100))
+		plt.plot(self.x_axis,pu.smooth(self.data.rav, radius=100))
+		plt.plot(self.x_axis,pu.smooth(self.data.ra, radius=100))
+		plt.plot(self.x_axis,pu.smooth(self.data.rlive, radius=100))
+		plt.legend(['pos','lvel','avel','act','live'])
+		plt.ylabel('reward')
+		if self.x_axis_unit=="time":
+			plt.xlabel('Time elapsed(s)')
+		else:
+			plt.xlabel('Step')
+		plt.grid()
+		plt.show()
 
-plt.legend(['episode mean reward','episode mean length'])
-if x_axis_unit=="time":
-	plt.xlabel('Time elapsed(s)')
-else:
-	plt.xlabel('Step')
-plt.ylabel('reward')
-plt.grid()
-
-
-plt.figure('Aux params')
-plt.plot(x_axis,pu.smooth(data.rp, radius=100))
-plt.plot(x_axis,pu.smooth(data.rlv, radius=100))
-plt.plot(x_axis,pu.smooth(data.rav, radius=100))
-plt.plot(x_axis,pu.smooth(data.ra, radius=100))
-plt.plot(x_axis,pu.smooth(data.rlive, radius=100))
-plt.legend(['pos','lvel','avel','act','live'])
-plt.ylabel('reward')
-if x_axis_unit=="time":
-	plt.xlabel('Time elapsed(s)')
-else:
-	plt.xlabel('Step')
-plt.grid()
-plt.show()
-
-#print(data.shape())
-#print(data[0:])
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# t = np.arange(0.0, 2.0, 0.01)
-# s = 1 + np.sin(2*np.pi*t)
-# plt.plot(t, s)
-
-# plt.xlabel('time (s)')
-# plt.ylabel('voltage (mV)')
-# plt.title('About as simple as it gets, folks')
-# plt.grid(True)
-# plt.savefig("test.png")
-# plt.show()
+if __name__=="__main__":
+	arg_length = len(sys.argv)
+	print ("With # args={}".format(arg_length))
+	if (arg_length != 2):
+		print ("""
+		usage: python ./plot_csv.py arg0
+		arg0: csv log file path
+		""")
+	else:
+		print("Loading file from {}".format(sys.argv[1]))
+		myPlot=plotCsv(sys.argv[1])
+		myPlot.loadData()
+		myPlot.plotData()
